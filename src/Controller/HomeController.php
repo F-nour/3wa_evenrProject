0a401568bc80a07c563b2bb4f846f892e715c5eb
Nour-eddine <fattoumne@gmail.com>
2022-06-14 16:26:01 +0200

    20220614-lastEvents

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,14 @@ class HomeController extends \Library\Core\AbstractController
 
     public function index()
     {
-        $this->render('Home', 'home');
+        $this->render('Home', 'home', [
+            'lastsEvents' => $this->displayEvents(),
+        ]);
+    }
+
+    public function displayEvents()
+    {
+        $eventManager = new \Model\Manager\EventManager();
+        return $eventManager->getLastsEvents();
     }
 }
