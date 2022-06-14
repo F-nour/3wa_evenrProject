9f33464af881431529e5057a2f8d1faeef7e9b91
Nour-eddine <fattoumne@gmail.com>
2022-06-14 15:12:16 +0200

    20220614-manager

--- src/Model/Manager/EventManager.php
+++ src/Model/Manager/EventManager.php
@@ -20,4 +20,10 @@ class EventManager extends \Library\Core\AbstractManager
         $result = $this->db->getResults('SELECT * FROM ' . SELF::EVENT . ' WHERE id_categorie = ' . $category . ' ORDER BY date DESC');
         return $result;
     }
+
+    public function addEvent(string $title, string $image_path, string $description, string $date, string $id_categorie)
+    {
+        $result = $this->db->execute('INSERT INTO ' . SELF::EVENT . ' (title, description, date, id_categorie) VALUES (?, ?, ?, ?, ?)', [$title, $image_path, $description, $date, $id_categorie]);
+        return $result;
+    }
 }
